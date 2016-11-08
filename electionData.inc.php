694d4b41640823980e3825f20314d48faac9cd39
LightsOnHudson <bshaver@MBPRO-TDL.local>
2016-11-08 10:11:42 -0700

    Added State and democrat

--- electionData.inc.php
+++ electionData.inc.php
@@ -2,8 +2,9 @@
 
 $VOTES_DATA_ARRAY = array(
 				//	array("US","http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Racewide_US.xml"),
-					array("DEMOCRAT","http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Races_Pres_Dem.xml"),
-					array("REPUBLICAN" ,"http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Races_Pres_Rep.xml"),
+				//primaries	array("DEMOCRAT","http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Races_Pres_Dem.xml"),
+				//primaries	array("REPUBLICAN" ,"http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Races_Pres_Rep.xml"),
+					array("DEMOCRAT","http://www.cbsnews.com/election-results-data/2016-General/xmlData/Races_Pres_Dem.xml"),
 					//append the STATE CODE 'CO' 'CA' to the end with .xml at the end of that in the getVOTES code
 				//	array("STATE","http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Racewide_")
 					array("PRESIDENTIAL","http://www.cbsnews.com/election-results-data/2016-General/xmlData/PresSummary.xml"),
