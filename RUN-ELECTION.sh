102c72a6f5c0c75fb802217bd5cd81126cb7fab1
LightsOnHudson <bshaver@MBPRO-TDL.local>
2016-11-08 10:42:06 -0700

    Added install for script

---
+++ RUN-ELECTION.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#Script to run randomizer
+#Created by MatrixMessage
+#
+/usr/bin/php /home/fpp/media/plugins/MatrixMessage/matrix.php
--- fpp_install.sh
+++ fpp_install.sh
@@ -2,4 +2,5 @@
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
 exec /opt/fpp/scripts/update_plugin ${target_PWD##*/}
+cp RUN-ELECTION.sh /home/fpp/media/scripts
 popd
