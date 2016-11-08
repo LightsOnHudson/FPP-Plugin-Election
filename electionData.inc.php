ed12e51e008bbc76a885ba527b553c6109c187ee
LightsOnHudson <bshaver@MBPRO-TDL.local>
2016-11-08 09:59:09 -0700

    Added battle ground
    States: Battleground

--- electionData.inc.php
+++ electionData.inc.php
@@ -6,7 +6,8 @@ $VOTES_DATA_ARRAY = array(
 					array("REPUBLICAN" ,"http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Races_Pres_Rep.xml"),
 					//append the STATE CODE 'CO' 'CA' to the end with .xml at the end of that in the getVOTES code
 				//	array("STATE","http://www.cbsnews.com/election-results-data/2016-Primaries/xmlData/Racewide_")
-					array("PRESIDENTIAL","http://www.cbsnews.com/election-results-data/2016-General/xmlData/PresSummary.xml")
+					array("PRESIDENTIAL","http://www.cbsnews.com/election-results-data/2016-General/xmlData/PresSummary.xml"),
+					array("BATTLEGROUND","http://www.cbsnews.com/election-results-data/2016-General/xmlData/Battleground.xml")
 					);
 
 
